7274b6e77fa2e5176793bf0508fd3912a5cfc71c
Michael Noronha <michaeltnoronha@gmail.com>
2018-02-11 04:12:32 -0600

    Add more traces

--- src/generate_traces.py
+++ src/generate_traces.py
@@ -10,10 +10,9 @@ if not os.path.isdir("out/nam"):
 protocol_pairs = [("Reno", "Reno"),
                   ("Newreno", "Reno"),
                   ("Vegas", "Vegas"),
-                  ("Newreno", "Vegas")
-                 ]
+                  ("Newreno", "Vegas")]
 
+# For each protocol pair, generate traces for each CBR from 1 to 40 with steps of 2
 for p1, p2 in protocol_pairs:
-    # for cbr in range(1,10,0.1):
-    os.system("ns general.tcl {} {} 1".format(p1,p2))
-    # print("ns general.tcl {} {} 1".format(p1,p2))
+    for cbr in range(1,40,2):
+        os.system("ns general.tcl {} {} {}".format(p1,p2, cbr))
