3419cee3d95524971500be6d2b22b6536d75f4ef
Michael Noronha <michaeltnoronha@gmail.com>
2018-02-12 11:56:36 -0600

    Generate dat files for two-flows accross cbr link

--- src/generate_data.py
+++ src/generate_data.py
@@ -6,21 +6,40 @@ import argparse
 import pathlib
 import glob
 import sys
-
-RELATIVE_PATH = sys.argv[0]
+import os
 
 
 # Files is an iterator over names of matching files
-def drops_vs_cbr(files):
+def drops_vs_cbr(files, args):
+    filepath = pathlib.Path(__file__).resolve().parent
+    if args.outfile:
+        out = open(args.outfile, "w")
+    elif args.rr:
+        out = open(os.path.join(filepath, "dat", "rr_part1.dat"), "w")
+    elif args.nrr:
+        out = open(os.path.join(filepath, "dat", "nrr_part1.dat"), "w")
+    elif args.vv:
+        out = open(os.path.join(filepath, "dat", "vv_part1.dat"), "w")
+    elif args.nrv:
+        out = open(os.path.join(filepath, "dat", "nrv_part1.dat"), "w")
+
     for f in files:
         loss_rate = 0
-        cbr = int(''.join(c for c in f if c.isdigit()))
 
-        with open(f) as o:
+        last_ind = f.rfind("/")
+        if last_ind != -1:
+            filename = f[last_ind+1:]
+        else:
+            filename = f
+
+        # Extract the CBR from the filename
+        cbr = int(''.join(c for c in filename if c.isdigit()))
+
+        with open(f) as opened:
             drops = 0
             recvs = 0
 
-            for line in o:
+            for line in opened:
                 if line[0] == "r":
                     recvs += 1
                 elif line[0] == "d":
@@ -28,26 +47,23 @@ def drops_vs_cbr(files):
 
             loss_rate = drops/(recvs + drops)
 
-        print("cbr: {}, loss_rate: {}".format(cbr, loss_rate))
+        out.write("{}\t{}\n".format(cbr, loss_rate))
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description="Generate input data for gnuplot.")
 
-    group = parser.add_mutually_exclusive_group()
+    group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("--rr", action="store_true")
     group.add_argument("--nrr", action="store_true")
     group.add_argument("--vv", action="store_true")
     group.add_argument("--nrv", action="store_true")
 
+    parser.add_argument("--outfile")
+
     args = parser.parse_args()
 
     if args.rr:
         file_pattern = "Reno_Reno_*.tr"
-        # with open("out/tr/Reno_Reno_13.tr") as f:
-        #     for line in f:
-        #         print(line)
-        #         print("hi")
-
     elif args.nrr:
         file_pattern = "Newreno_Reno_*.tr"
     elif args.vv:
@@ -59,6 +75,4 @@ if __name__=="__main__":
     gglob = "{}/out/tr/{}".format(filepath, file_pattern)
 
     file_matches = glob.iglob(gglob)
-    drops_vs_cbr(file_matches)
-
-
+    drops_vs_cbr(file_matches, args)
